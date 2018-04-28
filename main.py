54a13477a24402a2faecdf803a754c1f133e7222
tombody <tombody@gmail.com>
2018-04-28 13:36:13 +1000

    Main window working

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from tkinter import *
 import tkinter.messagebox
 
-TITLE = "Thomas Body - U1101544"
+TITLE = "K-Means - Thomas Body - U1101544"
+
 
 class Interface:
-    def __inti__(self, master):
+    def __init__(self, master):
         # Sets root title and min window size
         self.master = master
         master.title(TITLE)
         master.minsize(width=500, height=40)
+
+
+# Runs the main root display
+if __name__ == "__main__":
+    root = Tk()
+    interface = Interface(root)
+    root.mainloop()
